4ef9eb44f8e6e19fd1592a7e12a3af7539f09602
Kanagat01 <skanagat101@gmail.com>
2024-07-12 14:53:39 +0500

    hotfix

--- django_project/backend/settings.py
+++ django_project/backend/settings.py
@@ -40,7 +40,6 @@ EMAIL_HOST = os.environ.get("EMAIL_HOST")
 EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
 EMAIL_PORT = int(os.environ.get("EMAIL_PORT"))
-EMAIL_USE_TLS = bool(os.environ.get("EMAIL_USE_TLS"))
 EMAIL_USE_SSL = bool(os.environ.get("EMAIL_USE_SSL"))
 
 INSTALLED_APPS = [
