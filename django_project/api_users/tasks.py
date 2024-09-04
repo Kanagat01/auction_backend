57b2978451507e776317866d8d2ac31f3fb5a594
Kanagat01 <skanagat101@gmail.com>
2024-09-04 15:06:58 +0500

    hotfix

--- django_project/api_users/tasks.py
+++ django_project/api_users/tasks.py
@@ -6,8 +6,10 @@ from api_users.models import UserModel, TransporterCompany, CustomerCompany
 
 
 def monthly_deduct_subscription_fee():
-    t_companies = TransporterCompany.objects.filter(subscription__isnull=False)
-    c_companies = CustomerCompany.objects.filter(subscription__isnull=False)
+    t_companies = TransporterCompany.objects.filter(
+        subscription__isnull=False, user__subscription_paid=True)
+    c_companies = CustomerCompany.objects.filter(
+        subscription__isnull=False, user__subscription_paid=True)
 
     for company in [*t_companies, *c_companies]:
         subscription_price = company.subscription.price
